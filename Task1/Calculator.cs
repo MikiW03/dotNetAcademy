79fef227b02d113995dcca965f54181c700c9a76
Mikołaj Winiecki <mikiwiniecki@gmail.com>
2023-11-30 20:05:57 +0100

    Create a Calculator class

---
+++ Task1/Calculator.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1
+{
+    internal class Calculator
+    {
+        public static double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        public static double Subtract(double a, double b)
+        {
+            return a - b;
+        }
+
+        public static double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+
+        public static double Divide(double a, double b)
+        {
+            return a / b;
+        }
+
+        public static double Exponentiate(double a, double b)
+        {
+            return Math.Pow(a, b);
+        }
+
+        public static int Factorial(int a)
+        {
+            if (a == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return a * Factorial(a - 1);
+            }
+        }
+    }
+}
