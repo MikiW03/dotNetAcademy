7a6295a9c30663e3edd8e2ac3a123f8bb665c31d
MikiW03 <mikiwiniecki@gmail.com>
2023-12-28 17:26:06 +0100

    Create LeasingCompany class

---
+++ Task2/LeasingCompany.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    internal class LeasingCompany
+    {
+        private readonly List<Vehicle> _vehiclesList = [];
+        public List<Vehicle> GetVehicles()
+        {
+            return _vehiclesList;
+        }
+        public void AddVehicle(Vehicle vehicle)
+        {
+            _vehiclesList.Add(vehicle);
+        }
+
+
+        public List<Vehicle> GetExploitedVehicles()
+        {
+            return _vehiclesList;
+        }
+        public double GetTotalFleetValue()
+        {
+            return 0;
+        }
+        public List<Vehicle> GetMatchingVehicles(string brand, string color)
+        {
+            return _vehiclesList;
+        }
+        public List<Vehicle> GetVehiclesCloseToMaintanance()
+        {
+            return _vehiclesList;
+        }
+    }
+}
