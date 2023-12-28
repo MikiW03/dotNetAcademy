5579e8d1b53cdbc50a192dcc5bdd0f7f06eaf96c
MikiW03 <mikiwiniecki@gmail.com>
2023-12-28 16:50:03 +0100

    Create Cargo class

---
+++ Task2/Cargo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    public class Cargo(string brand, string model, int year, string color, decimal price, string registrationNumber) 
+        :Vehicle(brand, model, year, color, price, registrationNumber)
+    {
+        protected override int MileageUntilExploited { get; set; } = 1_000_000;
+        protected override int MileageUntilMaintanance { get; set; } = 15_000;
+        protected override int YearsUntilExploited { get; set; } = 15;
+        protected override int ValueDecreasePerYear { get; set; } = 7;
+
+        public decimal GetRentalCosts(DateTime endOfRental, int distance, int weight)
+        {
+            return 0;
+        }
+    }
+}
--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,9 +1,16 @@
-﻿namespace Task2
+﻿using System.Reflection;
+
+namespace Task2
 {
     internal class Program
     {
         static void Main()
         {
+            var car1 = new Cargo("Volvo", "FH16", 2019, "White", 100_000, "AB1234CD");
+            var car2 = new Cargo("Mercedes", "FH16", 2019, "White", 100_000, "AB1234CD");
+
+            Console.WriteLine(car1);
+            Console.WriteLine(car2);
         }
     }
 }
