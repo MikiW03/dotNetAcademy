51bb316fbbed96f42184ff75dfa7e5f410ecd2e7
MikiW03 <mikiwiniecki@gmail.com>
2024-01-05 16:58:30 +0100

    Override Vehicle string representation

--- Task2/Vehicle.cs
+++ Task2/Vehicle.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Metrics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +9,10 @@ namespace Task2
 {
     public abstract class Vehicle(string brand, string model, int year, string color, decimal price, string registrationNumber, int mileage, int comfortClass, double modelCoefficient)
     {
+        private static int counter = 0;
+
         public readonly Guid Id = Guid.NewGuid();
+        public readonly int Number = counter++;
         public string Brand { get; set; } = brand;
         public string Model { get; set; } = model;
         public int Year { get; set; } = year;
@@ -23,5 +27,22 @@ namespace Task2
         public abstract int MileageUntilMaintanance { get; protected set; }
         public abstract int YearsUntilExploited { get; protected set; }
         public abstract int ValueDecreasePerYear { get; protected set; }
+
+
+        public override string ToString()
+        {
+            var type = GetType().Name;
+            return String.Format($"No.{Number}{Environment.NewLine}" +
+                                 $"Type: {type}{Environment.NewLine}" +
+                                 $"Brand: {Brand}{Environment.NewLine}" +
+                                 $"Model: {Model}{Environment.NewLine}" +
+                                 $"Year of Manufacture: {Year}{Environment.NewLine}" +
+                                 $"Color: {Color}{Environment.NewLine}" +
+                                 $"Price: {Price}{Environment.NewLine}" +
+                                 $"Registration Number: {RegistrationNumber}{Environment.NewLine}" +
+                                 $"Mileage: {Mileage}{Environment.NewLine}" +
+                                 $"Comfort Class: {ComfortClass}{Environment.NewLine}" +
+                                 $"Model Coefficient: {ModelCoefficient}{Environment.NewLine}");
+        }
     }
 }
