55e02c66d9588ebe8bb05a62618fa63dd8d41664
MikiW03 <mikiwiniecki@gmail.com>
2024-01-05 16:52:21 +0100

    Add sample company initializer

---
+++ Task2/CompanyInitializer.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Reflection;
+using System.Diagnostics.Metrics;
+
+namespace Task2
+{
+    static class CompanyInitializer
+    {
+        public static LeasingCompany InitSampleCompany()
+        {
+            var leasingCompany = new LeasingCompany();
+
+            string filePath = Path.GetFullPath(Path.Combine(Environment.CurrentDirectory, @"..\..\..\")) + @"data\leasingCompany.json";
+            string passengerJson = File.ReadAllText(filePath);
+            var passenger = JsonConvert.DeserializeObject<List<Passenger>>(passengerJson);
+            foreach (var passengerVehicle in passenger)
+            {
+                leasingCompany.AddVehicle(passengerVehicle);
+            }
+
+            string filePath2 = Path.GetFullPath(Path.Combine(Environment.CurrentDirectory, @"..\..\..\")) + @"data\leasingCompany2.json";
+            string cargoJson = File.ReadAllText(filePath2);
+            var cargo = JsonConvert.DeserializeObject<List<Cargo>>(cargoJson);
+            foreach (var cargoVehicle in cargo)
+            {
+                leasingCompany.AddVehicle(cargoVehicle);
+            }
+
+            return leasingCompany;
+        }
+    }
+}
