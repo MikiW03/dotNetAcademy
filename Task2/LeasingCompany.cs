3780b51293bc3838a19eaccb38ccc75a8733f935
MikiW03 <mikiwiniecki@gmail.com>
2024-01-06 15:40:20 +0100

    Correct LeasingCompany methods to be in line with the task

--- Task2/LeasingCompany.cs
+++ Task2/LeasingCompany.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,11 +23,18 @@ namespace Task2
 
         public List<Vehicle> GetExploitedVehicles()
         {
-            return _vehiclesList.FindAll(vehicle => vehicle.Mileage > vehicle.MileageUntilExploited);
+            return _vehiclesList.FindAll(vehicle => vehicle.Mileage > vehicle.MileageUntilExploited 
+            ||  (DateTime.Now.Year - vehicle.Year) > vehicle.YearsUntilExploited);
         }
         public decimal GetTotalFleetValue()
         {
-            return _vehiclesList.Sum(vehicle => vehicle.Price);
+            return _vehiclesList.Sum(vehicle => vehicle.Price * (decimal)Math.Pow(1 - (double)vehicle.ValueDecreasePerYear / 100, DateTime.Now.Year - vehicle.Year));
+        }
+        public List<Vehicle> GetMatchingVehicles(string brand)
+        {
+            return _vehiclesList
+                .FindAll(vehicle => vehicle.Brand == brand)
+                .ToList();
         }
         public List<Vehicle> GetMatchingVehicles(string brand, string color)
         {
