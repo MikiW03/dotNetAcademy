bae53bb54c95ebd0e410190414e8d1a2013da2e4
MikiW03 <mikiwiniecki@gmail.com>
2024-01-07 20:46:27 +0100

    Add program initialization

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -6,16 +6,8 @@ namespace Task2
     {
         static void Main()
         {
-            var company = new LeasingCompany();
-            
-            company.AddVehicle(new Passenger("BMW1", "X5", 2015, "Black", 100_000, "BG1234", 4_999, 5));
-            company.AddVehicle(new Passenger("BMW2", "X5", 2015, "Black", 100_000, "BG1234", 3_999, 5));
-            company.AddVehicle(new Cargo("Mercedes1", "Sprinter", 2018, "White", 150_000, "BG4321", 29_900, 2));
-            company.AddVehicle(new Cargo("Mercedes2", "Sprinter", 2018, "White", 150_000, "BG4321", 28_900, 2));
-            company.AddVehicle(new Cargo("BMW", "Sprinter", 2018, "Black", 150_000, "BG4321", 1, 2));
-            company.AddVehicle(new Cargo("BMW", "Sprinter", 2018, "Black", 150_000, "BG4321", 1_000_001, 4));
-
-            var vehiclesCloseToMaintanance = company.GetVehiclesCloseToMaintanance();
+            var consoleHandler = new ConsoleMenuHandler();
+            consoleHandler.HandleUserInput();
         }
     }
 }
